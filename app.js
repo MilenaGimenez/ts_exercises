a643ca0889547bb593d9253e7279bbd0c001fcce
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-04-19 18:19:23 -0300

    resolución ejercicio 2

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 "use strict";
-// Ejercicio 1 - página 26:
-//Escribe un programa que a partir de un número entero de tres dígitos entregue el número con los dígitos en orden inverso:
 Object.defineProperty(exports, "__esModule", { value: true });
+// --------------------------------------------- Ejercicio 1 - página 26:
+//Escribe un programa que a partir de un número entero de tres dígitos entregue el número con los dígitos en orden inverso:
+//Solución 1
 // let numOriginal: number = 145;
 // let numString: string = numOriginal.toString()
 // let numArray: Array<string> = numString.split('').reverse()
 // let numUnir: string = numArray.join('')
 // console.log(numUnir)
 //Solución 2
-var invertirNumero = function (numero) {
-    var numString = numero.toString();
-    var numArray = numString.split('').reverse();
-    var numUnir = numArray.join('');
-    console.log("El n\u00FAmero inicial es: ".concat(numero, ". El n\u00FAmero invertido es: ").concat(numUnir));
-};
-invertirNumero(145);
+// const invertirNumero = (numero: number) => {
+//     let numString: string = numero.toString()
+//     let numArray: Array<string> = numString.split('').reverse()
+//     let numUnir: string = numArray.join('')
+//     console.log(`El número inicial es: ${numero}. El número invertido es: ${numUnir}`)
+// };
+// invertirNumero(145);
+//--------------------------------------------- Ejercicio 2 - página 26:
+//Escribe un programa a partir de una hora “actual” y un número entero de horas e indique qué hora marcará el reloj dentro de esa cantidad de horas.
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// rl.question('Ingrese la hora actual: ', (nombre: string) => {
+//   console.log(`Hola, ${nombre}!`);
+//   rl.close();
+// });
+// ;
+var horaActual = 11;
+var cantHoras = 43;
+//let horaFinal: number = 6;
+// for (let i = 1; i < cantHoras; i++) {
+//     // const element = array[i];
+//     console.log(i)
+//     if (i === 24) {
+//         console.log("estoy en 24")
+//         console.log(i[24])
+//     }
+// }
+var horaFinal = (horaActual + cantHoras) % 24;
+console.log(horaFinal);
+console.log("En ".concat(cantHoras, " horas, el reloj marcar\u00E1 las ").concat(horaFinal));
--- dist/app.js
+++ dist/app.js
@@ -1,50 +1,19 @@
-import * as readline from 'readline';
-
+"use strict";
 // --------------------------------------------- Ejercicio 1 - página 26:
 //Escribe un programa que a partir de un número entero de tres dígitos entregue el número con los dígitos en orden inverso:
-
 //Solución 1
 // let numOriginal: number = 145;
 // let numString: string = numOriginal.toString()
 // let numArray: Array<string> = numString.split('').reverse()
 // let numUnir: string = numArray.join('')
-
 // console.log(numUnir)
-
 //Solución 2
 // const invertirNumero = (numero: number) => {
 //     let numString: string = numero.toString()
 //     let numArray: Array<string> = numString.split('').reverse()
 //     let numUnir: string = numArray.join('')
-
 //     console.log(`El número inicial es: ${numero}. El número invertido es: ${numUnir}`)
 // };
-
 // invertirNumero(145);
-
-
 //--------------------------------------------- Ejercicio 2 - página 26:
 //Escribe un programa a partir de una hora “actual” y un número entero de horas e indique qué hora marcará el reloj dentro de esa cantidad de horas.
-
-//Solución 1
-let horaActual: number = 11;
-let cantHoras: number = 43;
-
-let horaFinal: number = (horaActual + cantHoras) % 24
-console.log(horaFinal)
-
-
-console.log(`En ${cantHoras} horas, el reloj marcará las ${horaFinal}`);
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.question('Ingrese la hora actual: ', (nombre: string) => {
-//   console.log(`Hola, ${nombre}!`);
-  
-//   rl.close();
-// });
-
-// ;
