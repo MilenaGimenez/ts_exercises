a9a21867499b2206fee1db375bbf589543ff3b4f
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-04-19 16:49:37 -0300

    resolución ejercicio 1

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hola mundo");
+// Ejercicio 1 - página 26:
+//Escribe un programa que a partir de un número entero de tres dígitos entregue el número con los dígitos en orden inverso:
+var numOriginal = 145;
+var numString = numOriginal.toString();
+var numArray = numString.split('').reverse();
+var numUnir = numArray.join('');
+console.log(numUnir);
+console.log(typeof numUnir);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("hola mundo")
+// Ejercicio 1 - página 26:
+//Escribe un programa que a partir de un número entero de tres dígitos entregue el número con los dígitos en orden inverso:
+
+let numOriginal: number = 145;
+let numString: string = numOriginal.toString()
+let numArray: Array<string> = numString.split('').reverse()
+let numUnir: string = numArray.join('')
+
+console.log(numUnir)
